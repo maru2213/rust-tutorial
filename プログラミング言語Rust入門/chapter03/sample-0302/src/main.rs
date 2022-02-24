0d804fba56eddf8e1f9ba6581779c4d6a0837b19
maru2213 <25478531+maru2213@users.noreply.github.com>
2022-02-24 17:59:05 +0900

    複合型

---
+++ プログラミング言語Rust入門/chapter03/sample-0302/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let name = "Name";
+    let age = 30;
+    let tuple = (name, age);
+    println!("{}, {}", tuple.0, tuple.1);
+
+    let seasons = ["春", "夏", "秋", "冬"];
+    println!("{}〜{}", seasons[0], seasons[3]);
+}
